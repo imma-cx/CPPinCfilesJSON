77f4189337658b2131998e9e4894751c4a153495
Emanuel Ribeiro <55888010+imma-cx@users.noreply.github.com>
2021-02-02 09:39:21 +0000

    Create test2.cs

---
+++ test/src/test2.cs
@@ -0,0 +1,12 @@
+using System;
+namespace printHelloCsharp
+ {
+   class HelloCsharp {
+      static void Main(string[] args) 
+      {
+         /* Print some string in C# */
+         Console.WriteLine("Hello C#.");
+         Console.ReadKey();
+      }
+   }
+}
